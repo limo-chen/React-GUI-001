70f8c0318fe1fd14e065f641b2ac91b573b53676
Calista <2320035651@qq.com>
2022-11-23 13:46:55 +0800

    webpack.config 分成两份

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import Button from "./button";
 
-ReactDOM.render(<Button></Button>, document.body);
+const a = 1;
+export { Button, a };
